b2317506ba1ab0da8e1ed846a19780095b742c0b
ivan_kovalev_1 <kovalevivan2004@yandex.ru>
2023-04-17 02:25:04 +0300

    все сделал

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        processor processor= new processor(34,12,"intel",456);
+        processor processor1= new processor(344,112,"Amd",46);
+        am am= new am(123,34,"GHH");
+        am am1= new am(12,3,"GHH1");
+        StorageofInformation StorageofInformation=new StorageofInformation(345,56,type.HDD);
+        StorageofInformation StorageofInformation1=new StorageofInformation(35,5,type.SSD);
+        Screen Screen = new Screen(23,45,TypeScreen.IPS);
+        Screen Screen1 = new Screen(223,145,TypeScreen.VA);
+        Keyboard Keyboard= new Keyboard("ter",true,12);
+        Keyboard Keyboard1= new Keyboard("er",false,124);
+        Computer Computer=new Computer(processor,am,StorageofInformation,Screen,Keyboard,"djf","ter");
+        System.out.println(Computer.Calculateweight(Computer));
+        Computer.print();
+        Computer.setAm(am1);
+        Computer.setKeyboard(Keyboard1);
+        Computer.setProcessor(processor1);
+        Computer.setScreen(Screen1);
+        Computer.setStorageofInformation(StorageofInformation1);
+        System.out.println();
+        System.out.println(Computer.Calculateweight(Computer));
+        Computer.print();
+    }
+}
---
+++ type.java
@@ -0,0 +1,3 @@
+public enum type {
+    HDD,SSD;
+}
