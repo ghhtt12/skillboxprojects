634d1472eacf1e5b7e2dec42b563407bf0e0317e
ivan_kovalev_1 <kovalevivan2004@yandex.ru>
2023-04-19 11:09:35 +0300

    Присвоил count значение равное 0,т.к. это класс ,а не примитив и по умолчанию он равен 0.Так же сделал вы вод всех кодов,только я не понимаю ,что за буква под кодом 1104,но она идет до ё,поэтому думаю нормально

--- Primitives/IntOrInteger/src/Container.java
+++ Primitives/IntOrInteger/src/Container.java
@@ -1,5 +1,7 @@
+package Primitives.IntOrInteger.src;
+
 public class Container {
-    private Integer count;
+    private Integer count=0;
 
     public void addCount(int value) {
         count = count + value;
--- Primitives/IntOrInteger/src/Main.java
+++ Primitives/IntOrInteger/src/Main.java
@@ -1,12 +1,17 @@
+package Primitives.IntOrInteger.src;
+
 public class Main {
     public static void main(String[] args) {
         Container container = new Container();
         container.addCount(5672);
         System.out.println(container.getCount());
-
-        // TODO: ниже напишите код для выполнения задания:
-        //  С помощью цикла и преобразования чисел в символы найдите все коды
-        //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
-
+        System.out.println("Коды букв русского алфавита:");
+        char a='Ё';
+        int s= (int)a;
+        System.out.println(s+"-"+a);
+        for (int i=1040;i<1106;i++){
+            char b=(char) i;
+            System.out.println(i+"-"+b);
+        }
     }
 }
