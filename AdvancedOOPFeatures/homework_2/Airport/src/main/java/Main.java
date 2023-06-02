0898eaff0117983b587a76e47be70b34fe1d737b
ivan_kovalev_1 <kovalevivan2004@yandex.ru>
2023-06-02 16:18:53 +0300

    сделал задания про аэропорты

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,17 +1,24 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import java.util.Collections;
-import java.util.List;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-
-
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
         //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+        Long now=System.currentTimeMillis()+(120*60000);
+        Date date1=new Date();
+        Date date= new Date(now);
+        List<Flight> res= airport.getTerminals().stream().flatMap(terminal -> terminal.getFlights().stream())
+                .filter(flight -> flight.getType().equals(Flight.Type.DEPARTURE)).
+                filter(flight -> flight.getDate().before(date)).
+                filter(flight -> flight.getDate().after(date1))
+                .collect(Collectors.toList());
+        return res;
     }
 
 }
